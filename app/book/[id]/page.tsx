83510af6a4fdf53795731127e860c526a2bd58b0
gozdedoner <gozde.doner.54@gmail.com>
2025-09-21 20:07:48 +0300

    fix: corrected Book import path in book/[id]/page.tsx

--- app/book/[id]/page.tsx
+++ app/book/[id]/page.tsx
@@ -3,7 +3,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from "next/navigation";
 import Image from "next/image";
-import { Book } from "../../../types";
+import { Book } from "../../types";
 
 export default function BookDetailPage() {
   const { id } = useParams<{ id: string }>();
