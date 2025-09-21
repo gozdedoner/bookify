71bfdf506420912af0147dbae84937b89a8a2fce
gozdedoner <gozde.doner.54@gmail.com>
2025-09-21 21:28:53 +0300

    chore: add Netlify build config

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,12 @@
 import type { NextConfig } from "next";
 
 const config: NextConfig = {
-  output: "export", // ✅ GitHub Pages için statik export
+  output: "export",
   images: {
-    unoptimized: true, // ✅ next/image hatalarını önlemek için
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "covers.openlibrary.org",
-        port: "",
-        pathname: "/b/id/**",
-      },
-    ],
+    unoptimized: true,
   },
-  basePath: "/bookify", // ✅ repo adını yaz (senin repo adı "bookify")
-  assetPrefix: "/bookify/",
+  basePath: "",
+  assetPrefix: "",
 };
 
 export default config;
